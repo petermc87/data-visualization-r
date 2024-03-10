2748084bcfaf42e16efcc1fffa1fc3190d78e170
petermc87 <peter.mcgibney@gmail.com>
2024-03-10 15:30:31 -0400

    feat: ggplot example 1

--- dataviz.R
+++ dataviz.R
@@ -1,9 +1,21 @@
-print("hello world")
+library(ggplot2)
 
+
+#  Tests
+print("hello world")
 y <- c(1, 2, 3)
 
+
+
+
+# Check the data available in R
 data()
-?BOD
+# ?BOD <-- Run this command in R terminal
 
 
 
+ggplot(data = BOD, # Dataset.
+        mapping = aes(x = Time, # Mapping the x and y axis. 
+                      y = demand))+
+                      geom_point(size = 5)+ # Displaying geometric points and specifying the size.
+                      geom_line(colour = "red") # Displaying a line and specifying the colour.
